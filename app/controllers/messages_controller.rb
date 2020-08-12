5ce9cbacdc1f692c9ec585dff116fc7adcd14e6e
keisukethewinter <addict20fx1@gmail.com>
2020-08-12 16:44:24 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,31 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+    #paramsに含まれているroom_idを代入します。
+# 紐解いて説明すると、直前の問題にあった通りルーティングをネストしているため/rooms/:room_id/messagesといったパスになります。
+# パスにroom_idが含まれているため、paramsというハッシュオブジェクトの中に、room_idという値が存在しています。そのため、params[:room_id]と記述することでroom_idを取得できます。
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+    #@room.messages.newでチャットルームに紐づいたメッセージのインスタンスを生成し、message_paramsを引数にして、privateメソッドを呼び出します。
+#その値を@messageに代入し、saveメソッドでメッセージの内容をmessagesテーブルに保存します。
+    
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+    #@message.saveでメッセージの保存に成功した場合、room_messages_path(@room)
+#（参加しているチャットルームに投稿したメッセージの一覧画面）にリダイレクトし、失敗した場合、indexアクションが実行され、同じページに戻るという記述をします。
+end
+
+private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    #パラメーターの中に、ログインしているユーザーのidと紐付いている、メッセージの内容(content)を受け取れるように許可します。
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root "rooms#index"
   resources :users,only: [:edit, :update]
-  resources :rooms,only: [:new,:create]
+  resources :rooms,only: [:new,:create] do
+    resources :messages, only: [:index, :create] #まず、メッセージ送信機能に必要なindexとcreateのルーティングを記述します。
+  #メッセージを投稿する際には、どのルームで投稿されたメッセージなのかをパスから判断できるようにしたいので、ルーティングのネストを利用します。
+#今回の場合、ネストをすることにより、roomsが親で、messagesが子という親子関係になるので、チャットルームに属しているメッセージという意味になります。
+#これによって、メッセージに結びつくルームのidの情報を含んだパスを、受け取れるようになります。
+#上記のルーティングを設定した後に、「rails routes」コマンドを実行しましょう。
+#URIに注目すると、ネストによって、/rooms/:room_id/messagesとなっています。これはメッセージに紐付いているチャットルームのidという意味です。
+#サイドバーのチャットルーム名に、 <%= link_to room.name, room_messages_path(room) %>とリンクを設定します。
+# パスにroomを引数とすることで、メッセージと紐付いているチャットルームのidを持たせることができます。
+# したがって、チャットルーム名をクリックすると、messagesコントローラーのindexアクションが実行され、チャットルームのページに遷移できます。
+
+
+end
 end
