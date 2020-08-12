647d31945e64971d46ff4788a999245f4000c182
keisukethewinter <addict20fx1@gmail.com>
2020-08-12 14:15:10 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,4 +4,20 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 
+  def create
+    @room = Room.create(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new #うまく行かなかった場合はrenderでrooms/new.html.erbにページを戻しています。
+    end
+  end
+
+
+
+
+  private
+  def room_params
+    params.require(:room).permit(:name, user_ids:[])#配列に対して保存を許可したい場合は、キーの名称と関連づけてバリューに「[]」と記述します。
+  end
 end
